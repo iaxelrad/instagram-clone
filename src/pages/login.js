adeced33afdd26bbd367f5215e2f6ad6a48c6e06
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-12 00:07:59 +0300

    basic Login page ui set up without functionality.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -27,7 +28,60 @@ export default function Login() {
           className="max-w-full"
         />
       </div>
-      <div className="flex w-2/5">I will be the form!</div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+            border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disable={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8
+            font-bold 
+            ${isInvalid && 'opacity-50'}`}
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+        <div className="flex flex-col justify-center w-full bg-white p-4 border border-gray-primary items-center">
+          <p className="text-sm ">
+            Don't have an account?{' '}
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
+//text-red-primary -> hex values
+//text-gray-base -> hex values
+//border-gray-primary
+//bg-blue-medium\
+//text-blue-medium
