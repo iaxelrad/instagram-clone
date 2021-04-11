42a7ec9ce1b465f5793a941f64b40bbc087b8bf6
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-11 08:46:20 +0300

    Initial Login page set up.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+
+const Login = lazy(() => import('./pages/login'));
+
 export default function App() {
-  return <p>Hello</p>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
