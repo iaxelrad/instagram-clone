18a476bdbd1f73a13cb4727db59f2affe4a45053
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-15 21:51:35 +0300

    Timeline  component created and grabbing images.

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,24 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
 export default function Timeline() {
+  //  getting logged in user's photos.
+  const { photos } = usePhotos();
+
+  // on loading the photos, we need to use react skeleton.
+  //if we have photos' render them ( a Post component)
+  // if the user has no photos, tell them to create some photos.
+
   return (
     <div className="container col-span-2">
-      <p>I am the timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map(content => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   );
 }
