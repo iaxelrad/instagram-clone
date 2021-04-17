d929bc028eb0afa78373b3d23798c73b50973e75
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-17 16:16:33 +0300

    Profile Header set.

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,15 +1,106 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import useUser from '../../hooks/use-user';
 import Skeleton from 'react-loading-skeleton';
+import { isUserFollowingProfile, toggleFollow } from '../../services/firebase';
 
 export default function Header({
   photosCount,
-  profile,
   followerCount,
   setFollowerCount,
+  profile: {
+    docId: profileDocId,
+    userId: profileUserId,
+    fullName,
+    followers = [],
+    following = [],
+    username: profileUsername,
+  },
 }) {
+  const { user } = useUser();
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
-  return null;
+  const activeBtnFollow = user.username && user.username !== profileUsername;
+
+  const handleToggleFollow = async () => {
+    setIsFollowingProfile(isFollowingProfile => !isFollowingProfile);
+    setFollowerCount({
+      followerCount: isFollowingProfile ? followerCount - 1 : followerCount + 1,
+    });
+    await toggleFollow(
+      isFollowingProfile,
+      user.docId,
+      profileDocId,
+      profileUserId,
+      user.userId
+    );
+  };
+
+  useEffect(() => {
+    const isLoggedInUserFollowingProfile = async () => {
+      const isFollowing = await isUserFollowingProfile(
+        user.username,
+        profileUserId
+      );
+      setIsFollowingProfile(!!isFollowing);
+    };
+
+    if (user.username && profileUserId) {
+      isLoggedInUserFollowingProfile();
+    }
+  }, [user.username, profileUserId]);
+
+  return (
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      <div className="container flex justify-center">
+        {user.username && (
+          <img
+            className="rounded-full h-40 w-40 flex"
+            alt={`${user.username} profile picture`}
+            src={`/images/avatars/${profileUsername}.jpg`}
+          />
+        )}
+      </div>
+      <div className="flex items-center justify-center flex-col col-span-2">
+        <div className="container flex items-center">
+          <p className="text-2xl mr-4">{profileUsername}</p>
+          {activeBtnFollow && (
+            <button
+              className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              onClick={handleToggleFollow}
+              onKeyDown={event => handleToggleFollow()}
+            >
+              {isFollowingProfile ? 'Unfollow' : 'Follow'}
+            </button>
+          )}
+        </div>
+        <div className="container flex mt-4">
+          {followers === undefined || following === undefined ? (
+            <Skeleton count={1} width={677} height={24} />
+          ) : (
+            <>
+              <p className="mr-10">
+                <span className="font-bold">{photosCount}</span> photos
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{followerCount}</span>{' '}
+                {followerCount === 1 ? 'follower' : 'followers'}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{following.length}</span> following
+              </p>
+            </>
+          )}
+        </div>
+        <div className="container mt-4">
+          <p className="font-medium">
+            {!fullName ? <Skeleton count={1} height={24} /> : fullName}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Header.propTypes = {
@@ -20,6 +111,8 @@ Header.propTypes = {
     docId: PropTypes.string,
     userId: PropTypes.string,
     fullName: PropTypes.string,
+    profileUsername: PropTypes.string,
+    followers: PropTypes.array,
     following: PropTypes.array,
   }).isRequired,
 };
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -53,9 +53,9 @@ export async function getSuggestedProfiles(userId, following) {
 }
 
 export async function updateLoggedInUserFollowing(
-  loggedInUserDocId,
-  profileId,
-  isFollowingProfile
+  loggedInUserDocId, //currently logged in user document id.
+  profileId, //the user that logged in user requests to follow.
+  isFollowingProfile // true/false - Am I following this person.
 ) {
   return firebase
     .firestore()
@@ -122,4 +122,48 @@ export async function getUserPhotosByUsername(username) {
     ...item.data(),
     docId: item.id,
   }));
+}
+
+export async function isUserFollowingProfile(
+  loggedInUserUsername,
+  profileUserId
+) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', loggedInUserUsername)
+    .where('following', 'array-contains', profileUserId)
+    .get();
+
+  const [response = {}] = result.docs.map(item => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return response.userId;
+}
+
+export async function toggleFollow(
+  isFollowingProfile,
+  activeUserDocId,
+  profileDocId,
+  profileUserId,
+  followingUserId
+) {
+  await updateLoggedInUserFollowing(
+    // Itamar's doc id
+    activeUserDocId,
+    // Raphaels user id
+    profileUserId,
+    //Is Itamar following Raphael - true/false
+    isFollowingProfile
+  );
+  await updateFollowedUserFollowers(
+    // Itamar's doc id
+    profileDocId,
+    // Raphaels user id
+    followingUserId,
+    //Is Itamar following Raphael - true/false
+    isFollowingProfile
+  );
 }
