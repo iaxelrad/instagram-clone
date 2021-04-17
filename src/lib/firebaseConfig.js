41eeee31a19229abc6329c277303feef217e2986
Itamar Axelrad <65605124+iaxelrad@users.noreply.github.com>
2021-04-18 00:59:09 +0300

    Delete firebaseConfig.js

--- src/lib/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBliJR1YzH1B6LvkBI-Fh5Y1dHHCli9Xzg',
-  authDomain: 'instagram-clone-52df1.firebaseapp.com',
-  projectId: 'instagram-clone-52df1',
-  storageBucket: 'instagram-clone-52df1.appspot.com',
-  messagingSenderId: '580333158820',
-  appId: '1:580333158820:web:f5b2b605a3908f11cb4131',
-};
