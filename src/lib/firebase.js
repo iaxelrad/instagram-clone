dbc0a2610505deb8dcf683f13fde041d62a3e3a7
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-18 01:01:17 +0300

    gitignore updates.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,7 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { firebaseConfig } from './firebaseConfig';
+import { firebaseConfig } from './config';
 // import { seedDatabase } from '../seed'; Don't uncomment or simply delete after first and only run
 
 const firebase = Firebase.initializeApp(firebaseConfig);
