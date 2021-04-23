b6bdd1636f47a7e145eb0dc74dd92d7e6ee24c1e
Itamar Axelrad <iaxelrad@gmail.com>
2021-04-23 17:16:49 +0300

    not found page test achieved full coverage.

---
+++ src/fixtures/logged-in-user.js
@@ -0,0 +1,9 @@
+export default {
+  userId: 'JNoMPWmBTpMcTdAcfvYV51hpODW2',
+  username: 'itamar',
+  fullName: 'Itamar Axelrad',
+  emailAddress: 'iaxelrad@gmail.com',
+  following: ['2'],
+  followers: ['2', '3', '4'],
+  dateCreated: 1613097964111,
+};
