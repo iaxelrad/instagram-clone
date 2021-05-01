f2b7c3038c039d0c6a59fc7bac010e624b7b9d52
Itamar Axelrad <iaxelrad@gmail.com>
2021-05-01 08:58:56 +0300

    work on testing the profile page.

--- src/__tests__/pages/dashboard.test.js
+++ src/__tests__/pages/dashboard.test.js
@@ -54,7 +54,6 @@ describe('<Dashboard />', () => {
         getByAltText,
         getByTitle,
         getAllByText,
-        // getAllByAltText,
         getByTestId,
       } = render(
         <Router>
---
+++ src/fixtures/profile-followed-by-logged-in-user.js
@@ -0,0 +1,9 @@
+export default {
+  userId: '3',
+  username: 'orwell',
+  fullName: 'George Orwell',
+  emailAddress: 'george@donotfollow.com',
+  following: ['2'],
+  followers: ['JNoMPWmBTpMcTdAcfvYV51hpODW2'],
+  dateCreated: 1613097964107,
+};
